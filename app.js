b074a6dae3a971b0802d130ab1046dbaf18513f6
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-03-18 23:23:48 +0900

    [ENH] package 설정
    - 암호화 package 변경

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var mysql = require('mysql');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var MySQLStore = require('express-mysql-session')(session);
-var jsonParser = bodyParser.json();
-var bcrypt = require('bcrypt');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
+const crypto = require('crypto');
+
+const dbConfig = require('./config.js');
+const connection = mysql.createConnection(dbConfig.SQL);
+const secret = dbConfig.KEY.secret;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -25,40 +26,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Connection 객체 생성
-var connection = mysql.createConnection({
-  host: '127.0.0.1',
-  port: 3306,
-  user: 'root',
-  password: '$manso1007',
-  database: 'manso_table'
-});
 
 // Connect
 connection.connect(function (err) {
-  if (err) {
-    console.error('mysql connection error');
-    console.error(err);
-    throw err;
-  }
+    if (err) {
+        console.error('mysql connection error');
+        console.error(err);
+        throw err;
+    }
 });
-
-var options = {
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '$manso1007',
-    database: 'manso_table'
-}
-var sessionStore = new MySQLStore(options);
-app.use(session({
-    secret: '!@#$%^&*',  // 암호화
-    resave: false,
-    saveUninitialized: true,
-    store: sessionStore
-}));
 
 app.use('/', indexRouter);
 app.use('/api/users', cors(), usersRouter);
@@ -66,18 +48,18 @@ app.use('/api/slides', slidesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
