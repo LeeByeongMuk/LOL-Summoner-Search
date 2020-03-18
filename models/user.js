f06455d0d22322b422e549bf913be7701a746343
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-03-18 23:24:30 +0900

    [ADD] user model 추가

---
+++ models/user.js
@@ -0,0 +1,7 @@
+class User {
+    constructor(user_id, user_password, user_name){
+        this.user_id = user_id;
+        this.user_password = user_password;
+        this.user_name = user_name;
+    }
+}
