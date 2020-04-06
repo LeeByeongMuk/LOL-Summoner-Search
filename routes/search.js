cdc6684825ed2387dab609f403388f5294a9c526
LeeByeongMuk <32323158+LeeByeongMuk@users.noreply.github.com>
2020-04-06 23:06:15 +0900

    [CLN] 검색 코드 분리

--- routes/search.js
+++ routes/search.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const request = require('request');
 const urlencode = require('urlencode');
 
-const key = 'RGAPI-a6f4aa29-de84-439a-9560-594d238e4cd6';
+const key = 'RGAPI-d09d4231-3d7f-45a8-8bd8-c0061f884dea';
 
 router.post('/id', function(req, res){
     let summonerName = req.body.summonerName;
@@ -16,20 +16,24 @@ router.post('/id', function(req, res){
         const data = JSON.parse(body);
 
         if (data.status == '' || data.status == null || data.status == undefined) {
-            url = `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${urlencode(data.id)}?api_key=${key}`;
-
-            request(url, function(err2, response2, body2){
-                if (err2) {
-                    console.log(err2);
-                }
-
-                res.json(JSON.parse(body2));
-            });
+            res.json(data)
         } else {
             res.json({
                 message: '검색 결과가 없습니다.'
             })
         }
+    });
+});
+
+router.post('/rank', function(req, res) {
+    let url = `https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${urlencode(req.body.id)}?api_key=${key}`;
+
+    request(url, function(err2, response2, body2){
+        if (err2) {
+            console.log(err2);
+        }
+
+        res.json(JSON.parse(body2));
     });
 });
 
